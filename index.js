eea775ca6eaaa1b0ee1c6ed346c0a272a0493410
Vivekjaiswal43348 <vivekjaiswal43348@gmail.com>
2025-01-12 19:38:15 +0530

    Node JS HTTP Serer with multiple Http path or node js routes

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-let http = require('http');
+let http = require("http");
 
-let server = http.createServer((req, res)=>{
-    // in node js
-    res.end("Hello, I am from node server!"); 
-    /**
-     * in expressjs
-     * res.send("Hello"); 
-     */
+let server = http.createServer((req, res) => {
+	console.log("Current URL:", req.url);
+	// simple node js routing
+	if (req.url === "/") { // http://localhost:8080
+		// in node js
+		res.end("Hello, I am from node server!");
+		/**
+		 * in expressjs
+		 * res.send("Hello");
+		 */
+	}
+	if (req.url === "/about") { // http://localhost:8080/about
+        let aboutData = {
+            gender: 'male',
+            name: 'Vijay',
+            location: 'Mumbai',
+            desc: "Hi, This is about me page!"
+        }
+		res.end(JSON.stringify(aboutData));
+	}
+	if (req.url === "/course") { // http://localhost:8080/course
+		res.end("This is my product course page!");
+	}
 });
 
 server.listen(8080); // http://localhost:8080
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 ## Setting up Node JS HTTP Serer:
 1. Node JS HTTP Serer at port 8080 with Single Http path
+2. Node JS HTTP Serer with multiple Http path or node js routes
